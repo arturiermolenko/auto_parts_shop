e2613102b4b25c55634837491476c1204524558a
artur <arturiermolenko@gmail.com>
2024-04-29 21:32:38 +0200

    added to admin panel opportunity to add many images of the item

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Item, Category
+from .models import Item, Category, ItemImage
 
-admin.site.register(Item)
+
+class ItemImageInline(admin.TabularInline):
+    model = ItemImage
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [ItemImageInline]
+
+
 admin.site.register(Category)
