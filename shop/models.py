6867d9ef63860132ae3828e775f29bcfe007c4f3
arturiermolenko <arturiermolenko@gmail.com>
2024-04-29 12:00:32 +0200

    created model Item

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    number = models.CharField(max_length=6, unique=True)
+    description = models.TextField()
+    materials = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    costs_prise = models.DecimalField(max_digits=16, decimal_places=2)
+    sell_price = models.DecimalField(max_digits=16, decimal_places=2)
+    link_where_buy = models.TextField()
+    
+    class Meta:
+        ordering = ["number"]
+
+    def __str__(self):
+        return f"{self.number} {self.name}"
+    
