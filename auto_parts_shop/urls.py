58ea0cec40766c6d93024c950e6ba0ab8b9433eb
artur <arturiermolenko@gmail.com>
2024-05-02 07:00:10 +0200

    added first test-view, created shop.urls.py and connected it as well as templates folder

--- auto_parts_shop/urls.py
+++ auto_parts_shop/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = ([
     path("admin/", admin.site.urls),
-]
+    path("", include("shop.urls", namespace="shop")),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
